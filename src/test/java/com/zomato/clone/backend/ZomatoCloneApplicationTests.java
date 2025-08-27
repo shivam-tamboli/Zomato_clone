5d9b7cd39b5c2e4564bde06305039d3376f99978
Shivam <shivamtamboli66@gmail.com>
2025-08-27 16:47:18 +0530

    initial commit

---
+++ src/test/java/com/zomato/clone/backend/ZomatoCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zomato.clone.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZomatoCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
