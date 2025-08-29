891062e0ec3cf5f355a14407a806bd65d70087e8
Shivam <shivamtamboli66@gmail.com>
2025-08-29 23:24:04 +0530

    Add Restaurant entity with fields and OneToMany relationships to FoodItem and RestaurantImages. Closes #3

---
+++ src/main/java/com/zomato/clone/backend/models/FoodItem.java
@@ -0,0 +1,39 @@
+package com.zomato.clone.backend.models;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "food_items")
+public class FoodItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "food_item_id", nullable = false)
+    private Integer fooditemid;
+
+    @Column(name = "food_name", nullable = false)
+    private String name;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "image", nullable = false)
+    private String image;
+
+    @Column(name = "food_item_rating", nullable = false)
+    private Double fooditemrating = 0.0;
+
+    @Column(name = "number_of_rating")
+    private Integer numofrating = 0;
+
+    @JsonBackReference
+    @ManyToOne
+    @JoinColumn(name = "restaurant_id")
+    private RestaurantInfo restaurantInfo;
+
+}
