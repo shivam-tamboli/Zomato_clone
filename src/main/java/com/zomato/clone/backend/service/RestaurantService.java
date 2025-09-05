8bd8a8c1cec5495e70b116e380df05a422e4c558
Shivam <shivamtamboli66@gmail.com>
2025-09-05 19:57:12 +0530

    Add getRestaurants() method which initial repository fetch logic in RestaurantServices. Closes #22

--- src/main/java/com/zomato/clone/backend/service/RestaurantService.java
+++ src/main/java/com/zomato/clone/backend/service/RestaurantService.java
@@ -1,8 +1,14 @@
 package com.zomato.clone.backend.service;
 
+import com.zomato.clone.backend.models.RestaurantDetails;
+import com.zomato.clone.backend.models.RestaurantInfo;
 import com.zomato.clone.backend.repository.RestaurantInfoRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Iterator;
 
 @Service
 public class RestaurantService {
@@ -13,5 +19,12 @@ public class RestaurantService {
     public RestaurantService(RestaurantInfoRepo restaurantInfoRepo) {
 
         this.restaurantInfoRepo = restaurantInfoRepo;
+    }
+
+    public ResponseEntity<ArrayList<RestaurantDetails>> getRestaurants(){
+
+        Iterable<RestaurantInfo> restaurants = restaurantInfoRepo.findAll();
+
+        return null;
     }
 }
