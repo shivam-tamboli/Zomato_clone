fa7bcfe3222ac80ed21d21cbd0956657129a85f0
Shivam <shivamtamboli66@gmail.com>
2025-09-05 15:44:20 +0530

    Implement constructor based dependency injection for RestaurantInfoRepo in RestaurantService. Closes #21

--- src/main/java/com/zomato/clone/backend/service/RestaurantService.java
+++ src/main/java/com/zomato/clone/backend/service/RestaurantService.java
@@ -1,8 +1,17 @@
 package com.zomato.clone.backend.service;
 
+import com.zomato.clone.backend.repository.RestaurantInfoRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RestaurantService {
 
+
+    private final RestaurantInfoRepo restaurantInfoRepo;
+
+    public RestaurantService(RestaurantInfoRepo restaurantInfoRepo) {
+
+        this.restaurantInfoRepo = restaurantInfoRepo;
+    }
 }
