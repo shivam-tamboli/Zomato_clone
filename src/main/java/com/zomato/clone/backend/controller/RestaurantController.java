9dd2102b246e14dbb72a403d6c23f02978c04202
Shivam <shivamtamboli66@gmail.com>
2025-09-06 21:04:29 +0530

    Use Constructor based DI for RestaurantService in RestaurantController. Closes #28

--- src/main/java/com/zomato/clone/backend/controller/RestaurantController.java
+++ src/main/java/com/zomato/clone/backend/controller/RestaurantController.java
@@ -1,7 +1,16 @@
 package com.zomato.clone.backend.controller;
 
+import com.zomato.clone.backend.service.RestaurantService;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RestaurantController {
+
+    private final RestaurantService restaurantService;
+
+    public RestaurantController(RestaurantService restaurantService){
+        this.restaurantService = restaurantService;
+    }
+
+
 }
