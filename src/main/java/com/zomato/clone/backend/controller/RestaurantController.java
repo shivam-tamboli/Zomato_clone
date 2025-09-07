ad41e239733c2b2e3aff3328d0c464056cc13c1b
Shivam <shivamtamboli66@gmail.com>
2025-09-07 14:37:20 +0530

    Add Get /zomato/get-restaurants endpoint in RestaurantController. Closes #29

--- src/main/java/com/zomato/clone/backend/controller/RestaurantController.java
+++ src/main/java/com/zomato/clone/backend/controller/RestaurantController.java
@@ -1,8 +1,13 @@
 package com.zomato.clone.backend.controller;
 
+import com.zomato.clone.backend.models.RestaurantDetails;
 import com.zomato.clone.backend.service.RestaurantService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping(value = "/zomato")
@@ -12,6 +17,11 @@ public class RestaurantController {
 
     public RestaurantController(RestaurantService restaurantService){
         this.restaurantService = restaurantService;
+    }
+
+    @GetMapping(value = "get-restaurants")
+    public ResponseEntity<ArrayList<RestaurantDetails>> getRestaurants(){
+        return restaurantService.getRestaurants();
     }
 
 
