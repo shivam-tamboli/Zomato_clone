2489aa017fedbd756b81dd11065eadb40d6dbe0f
Shivam <shivamtamboli66@gmail.com>
2025-09-07 14:21:03 +0530

    Add @RequestMapping in a RestaurantController class.

--- src/main/java/com/zomato/clone/backend/controller/RestaurantController.java
+++ src/main/java/com/zomato/clone/backend/controller/RestaurantController.java
@@ -1,9 +1,11 @@
 package com.zomato.clone.backend.controller;
 
 import com.zomato.clone.backend.service.RestaurantService;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/zomato")
 public class RestaurantController {
 
     private final RestaurantService restaurantService;
