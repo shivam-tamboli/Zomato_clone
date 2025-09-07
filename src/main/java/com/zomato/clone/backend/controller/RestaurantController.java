912076b46817e84d938b93fe6946d94a2842af33
Shivam <shivamtamboli66@gmail.com>
2025-09-07 15:01:04 +0530

    Add @PostMapping /zomato/get-restaurants in a RestaurantController, it fetches the fooditems using restaurantid. Closes #30

--- src/main/java/com/zomato/clone/backend/controller/RestaurantController.java
+++ src/main/java/com/zomato/clone/backend/controller/RestaurantController.java
@@ -1,13 +1,14 @@
 package com.zomato.clone.backend.controller;
 
+import com.zomato.clone.backend.models.FoodItem;
 import com.zomato.clone.backend.models.RestaurantDetails;
 import com.zomato.clone.backend.service.RestaurantService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/zomato")
@@ -22,6 +23,11 @@ public class RestaurantController {
     @GetMapping(value = "get-restaurants")
     public ResponseEntity<ArrayList<RestaurantDetails>> getRestaurants(){
         return restaurantService.getRestaurants();
+    }
+
+    @PostMapping(value = "get-fooditems")
+    public ResponseEntity<List<FoodItem>> getFoodItems(@RequestBody Map<String, Integer> entity){
+        return restaurantService.getFoodItems(entity);
     }
 
 
