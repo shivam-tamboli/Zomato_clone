70324edda5999b8e6168d108f629d3392b98d7b3
Shivam <shivamtamboli66@gmail.com>
2025-09-12 18:51:50 +0530

    Annotate with @RequestMapping with value. Closes #44

--- src/main/java/com/zomato/clone/backend/controller/UserController.java
+++ src/main/java/com/zomato/clone/backend/controller/UserController.java
@@ -3,9 +3,11 @@ package com.zomato.clone.backend.controller;
 import com.zomato.clone.backend.service.UserService;
 import com.zomato.clone.backend.service.ValidUser;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/zomato/user")
 public class UserController {
 
     private final ValidUser validUser;
