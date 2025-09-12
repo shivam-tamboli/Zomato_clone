06fbabc9c256179a5abb371baa4d3cd7d1ae28e0
Shivam <shivamtamboli66@gmail.com>
2025-09-12 18:47:47 +0530

    Implement Constructor based dependency injection in UserController. Closes #43

--- src/main/java/com/zomato/clone/backend/controller/UserController.java
+++ src/main/java/com/zomato/clone/backend/controller/UserController.java
@@ -1,8 +1,18 @@
 package com.zomato.clone.backend.controller;
 
+import com.zomato.clone.backend.service.UserService;
+import com.zomato.clone.backend.service.ValidUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+
+    private final ValidUser validUser;
+    private final UserService userService;
+
+    public UserController(ValidUser validUser, UserService userService) {
+        this.validUser = validUser;
+        this.userService = userService;
+    }
 }
