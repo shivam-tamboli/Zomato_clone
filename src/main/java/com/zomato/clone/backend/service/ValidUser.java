f38f9df7428a9d8da6eeb5a1cebab45143470c40
Shivam <shivamtamboli66@gmail.com>
2025-09-12 13:24:49 +0530

    Implement Constructor based Dependency Injection with UserInfoRepo in ValidUser. Closes #40

--- src/main/java/com/zomato/clone/backend/service/ValidUser.java
+++ src/main/java/com/zomato/clone/backend/service/ValidUser.java
@@ -1,7 +1,15 @@
 package com.zomato.clone.backend.service;
 
+import com.zomato.clone.backend.repository.UserInfoRepo;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ValidUser {
+
+    private final UserInfoRepo userInfoRepo;
+
+    public ValidUser(UserInfoRepo userInfoRepo) {
+        this.userInfoRepo = userInfoRepo;
+    }
+
 }
