3f7fa4b754417112e5150e4e637e5e02a4833f91
Shivam <shivamtamboli66@gmail.com>
2025-09-12 18:26:22 +0530

    Add validation in ValidUser. Closes #41

--- src/main/java/com/zomato/clone/backend/service/ValidUser.java
+++ src/main/java/com/zomato/clone/backend/service/ValidUser.java
@@ -1,15 +1,35 @@
 package com.zomato.clone.backend.service;
 
+import com.zomato.clone.backend.models.UserInfo;
 import com.zomato.clone.backend.repository.UserInfoRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Iterator;
 
 @Service
 public class ValidUser {
 
-    private final UserInfoRepo userInfoRepo;
+
+    private final  UserInfoRepo userInfoRepo;
 
     public ValidUser(UserInfoRepo userInfoRepo) {
         this.userInfoRepo = userInfoRepo;
     }
 
+    public boolean isPhoneNumberUnique(String phoneNumber){
+
+        Iterable<UserInfo> itr = userInfoRepo.findAll();
+
+        Iterator<UserInfo> i = itr.iterator();
+
+        while (i.hasNext()){
+
+            UserInfo userInfo = i.next();
+            if(phoneNumber.equals(userInfo.getPhoneNumber())){
+                return false;
+            }
+        }
+        return true;
+    }
 }
