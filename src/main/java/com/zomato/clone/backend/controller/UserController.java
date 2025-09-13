fc4c9903776a343222871422dc14a3d5bc108be2
Shivam <shivamtamboli66@gmail.com>
2025-09-13 17:53:06 +0530

    Comments

--- src/main/java/com/zomato/clone/backend/controller/UserController.java
+++ src/main/java/com/zomato/clone/backend/controller/UserController.java
@@ -8,18 +8,26 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
-@RestController
-@RequestMapping(value = "/zomato/user")
+@RestController// Marks this class as a REST controller (can handle HTTP requests & return JSON/XML)
+@RequestMapping(value = "/zomato/user")// Base URL for all user-related endpoints
 public class UserController {
 
     private final ValidUser validUser;
     private final UserService userService;
 
+    // Constructor-based dependency injection for ValidUser and UserService.
     public UserController(ValidUser validUser, UserService userService) {
         this.validUser = validUser;
         this.userService = userService;
     }
 
+
+    /*
+    Endpoint: POST /zomato/user/signup
+    signup API → creates a new user
+    checks if phone number already exists, if yes → return "phone"
+    else → forward data to service to save in DB
+    */
     @PostMapping("/signup")
     ResponseEntity<String> signup(@RequestBody Map<String, String> signupDetails){
      if(!validUser.isPhoneNumberUnique(signupDetails.get("phonenumber"))){
@@ -28,6 +36,15 @@ public class UserController {
      return userService.signUp(signupDetails);
     }
 
+
+   /*
+   Endpoint: POST /zomato/user/login
+   login API → authenticates existing user
+    flow:
+    1. check if phone exists, if not → "phone"
+    2. check password, if wrong → "password"
+    3. if all good → delegate to service (updates login status, returns success)
+   */
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody Map<String, String> loginDetails){
         if(validUser.isPhoneNumberUnique(loginDetails.get("phonenumber"))){
@@ -38,4 +55,5 @@ public class UserController {
         }
         return userService.login(loginDetails);
     }
+
 }
