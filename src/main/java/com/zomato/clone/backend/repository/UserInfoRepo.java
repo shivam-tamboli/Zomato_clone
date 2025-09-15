138a1e5ff0f7cac8e4e60256fdaba4e441682be6
Shivam <shivamtamboli66@gmail.com>
2025-09-15 19:54:10 +0530

    Annotate @Repository

--- src/main/java/com/zomato/clone/backend/repository/UserInfoRepo.java
+++ src/main/java/com/zomato/clone/backend/repository/UserInfoRepo.java
@@ -2,9 +2,10 @@ package com.zomato.clone.backend.repository;
 
 import com.zomato.clone.backend.models.UserInfo;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface UserInfoRepo extends CrudRepository<UserInfo, Integer> {
 
     public Optional<UserInfo> findByPhoneNumber(String phoneNumber);
