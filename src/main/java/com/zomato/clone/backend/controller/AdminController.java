6d2c49eab65b519e316733fd420b177cb7112efb
Shivam <shivamtamboli66@gmail.com>
2025-09-17 19:54:59 +0530

    Use constructor-based DI for AdminService in AdminController. Closes #71

--- src/main/java/com/zomato/clone/backend/controller/AdminController.java
+++ src/main/java/com/zomato/clone/backend/controller/AdminController.java
@@ -1,10 +1,17 @@
 package com.zomato.clone.backend.controller;
 
 
+import com.zomato.clone.backend.service.AdminService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/zomato/admin")
 public class AdminController {
+
+    private final AdminService adminService;
+
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
+    }
 }
