bdfdd7ae91b581e68fbffde36324584769257ffe
Shivam <shivamtamboli66@gmail.com>
2025-09-18 14:32:12 +0530

    Implement /add-restaurant POST API in AdminController to delegate restaurant creation to AdminService. Closes #72

--- src/main/java/com/zomato/clone/backend/controller/AdminController.java
+++ src/main/java/com/zomato/clone/backend/controller/AdminController.java
@@ -2,8 +2,13 @@ package com.zomato.clone.backend.controller;
 
 
 import com.zomato.clone.backend.service.AdminService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/zomato/admin")
@@ -13,5 +18,10 @@ public class AdminController {
 
     public AdminController(AdminService adminService) {
         this.adminService = adminService;
+    }
+
+    @PostMapping(value = "/add-restaurant")
+    public ResponseEntity<String> addRestaurant(@RequestBody Map entity){
+        return adminService.addRestaurant(entity);
     }
 }
